7253fac7eec826d12e44f3128d467ded30513d27
Diana Goering <goeringdi@gmail.com>
2023-04-07 23:49:19 +0200

    init

--- src/app.js
+++ src/app.js
@@ -7,31 +7,26 @@ const mongoose = require('mongoose');
 const userRouter = require('./routes/users');
 const bookRouter = require('./routes/books');
 
-
-
 dotenv.config();
 
 const { 
     PORT = 3005, 
     API_URL = 'http://127.0.0.1',
-    MONGO_URL = 'mongodb://127.0.0.1:27017/test'
+    MONGO_URL = 'mongodb://127.0.0.1:3005/library'
 } = process.env;
 
 mongoose.connect(MONGO_URL)
 
 const app = express();
 
+app.use(bodyParser.json());
+
+
 app.get('/', (request, response) => {
     response.status(200);
     response.send("Hello, World!");
 });
 
-// Middleware для обработки 404 ошибки
-app.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
-});
    
 // Middleware для обработки ошибок
 app.use((error, req, res, next) => {
@@ -47,7 +42,13 @@ app.use(cors);
 app.use('/', logOriginalUrl);
 app.use(userRouter);
 app.use(bookRouter);
-app.use(bodyParser.json());
+
+// Middleware для обработки 404 ошибки
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
 
 app.listen(PORT, () => {
     console.log(`Ссылка на сервер: ${API_URL}:${PORT}`);
